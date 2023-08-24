2df85734dfd5fd81f5ff4aba2e700369eefde940
Ling Samuel (WSL) <lingsamuelgrace@gmail.com>
2023-08-24 09:31:44 +0800

    test: add e2e test

---
+++ test/cli/suites/diff.go
@@ -0,0 +1,26 @@
+package suites
+
+import (
+	"bytes"
+	"os/exec"
+
+	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
+)
+
+var _ = ginkgo.Describe("`adc diff` tests", func() {
+	ginkgo.Context("Basic functions", func() {
+		_ = NewScaffold()
+		ginkgo.It("should return the diff result", func() {
+			var out bytes.Buffer
+			cmd := exec.Command("adc", "diff", "-f", "testdata/test.yaml")
+			cmd.Stdout = &out
+			err := cmd.Run()
+			gomega.Expect(err).To(gomega.BeNil())
+			gomega.Expect(out.String()).To(gomega.Equal(`creating service: "svc1"
+creating route: "route1"
+Summary: created 2, updated 0, deleted 0
+`))
+		})
+	})
+})
