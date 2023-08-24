66b4acfd8f986a1d37426135eb37d563a81321ff
Ling Samuel (WSL) <lingsamuelgrace@gmail.com>
2023-08-24 11:15:34 +0800

    fix: add placeholder for validate
    Signed-off-by: Ling Samuel (WSL) <lingsamuelgrace@gmail.com>

--- internal/pkg/validator/validator.go
+++ internal/pkg/validator/validator.go
@@ -43,6 +43,9 @@ func (v *Validator) Validate() []error {
 
 	for _, service := range v.localConfig.Services {
 		service := service
+		if service.ID == "" {
+			service.ID = "placeholder"
+		}
 		err := v.cluster.Service().Validate(context.Background(), service)
 		if err != nil {
 			allErr = append(allErr, err)
@@ -51,6 +54,9 @@ func (v *Validator) Validate() []error {
 
 	for _, route := range v.localConfig.Routes {
 		route := route
+		if route.ID == "" {
+			route.ID = "placeholder"
+		}
 		err := v.cluster.Route().Validate(context.Background(), route)
 		if err != nil {
 			allErr = append(allErr, err)
