80b7ddb7190c9013209ac98e2323adbb8972c7ac
Jintao Zhang <zhangjintao9020@gmail.com>
2023-09-04 23:43:08 +0800

    feat: add SSL types
    Signed-off-by: Jintao Zhang <zhangjintao9020@gmail.com>

---
+++ pkg/api/apisix/ssl.go
@@ -0,0 +1,26 @@
+package apisix
+
+import (
+	"context"
+
+	"github.com/api7/adc/pkg/api/apisix/types"
+)
+
+type sslClient struct {
+	*resourceClient[types.SSL]
+}
+
+func newSSL(c *Client) Consumer {
+	cli := newResourceClient[types.SSL](c, "ssls")
+	return &sslClient{
+		resourceClient: cli,
+	}
+}
+
+func (u *sslClient) Create(ctx context.Context, obj *types.SSL) (*types.SSL, error) {
+	return u.resourceClient.Create(ctx, obj.Username, obj)
+}
+
+func (u *sslClient) Update(ctx context.Context, obj *types.SSL) (*types.SSL, error) {
+	return u.resourceClient.Update(ctx, obj.Username, obj)
+}
