7e12837726fb44e2748ae10630eece2414a56936
Ling Samuel (WSL) <lingsamuelgrace@gmail.com>
2023-09-18 10:55:10 +0800

    test: add test case for sync-deletion usecase
    Signed-off-by: Ling Samuel (WSL) <lingsamuelgrace@gmail.com>

--- test/cli/config/ci_config.go
+++ test/cli/config/ci_config.go
@@ -1,10 +1,17 @@
 package config
 
-import "os"
+import (
+	"os"
+	"strings"
+)
 
 var (
 	TestUpstream = "httpbin.org"
 )
+
+func ReplaceUpstream(conf string) string {
+	return strings.ReplaceAll(conf, "HTTPBIN_PLACEHOLDER", TestUpstream)
+}
 
 func init() {
 	if os.Getenv("GITHUB_ACTIONS") == "true" {
