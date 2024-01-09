e609130d9e1e82d15c7f8213553d9e8af18b51d5
bzp2010 <bzp2010@apache.org>
2024-01-09 16:11:44 +0800

    debug

--- test/cli/suites-consumer/sync.go
+++ test/cli/suites-consumer/sync.go
@@ -29,6 +29,7 @@ var _ = ginkgo.Describe("`adc sync` tests", func() {
 					"time_window":   60,
 					"rejected_code": 503,
 					"key":           "remote_addr",
+					"_vid":          "suites-consumer",
 				},
 			},
 		}
