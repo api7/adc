855c6061c1f925ad5d3f405dc2261fb115edf6db
bzp2010 <bzp2010@apache.org>
2024-07-14 17:51:59 +0800

    ci: replace globalThis by process.env

--- libs/backend-api7/e2e/default-value.e2e-spec.ts
+++ libs/backend-api7/e2e/default-value.e2e-spec.ts
@@ -6,8 +6,8 @@ describe('Default Value', () => {
 
   beforeAll(() => {
     backend = new BackendAPI7({
-      server: globalThis.server,
-      token: globalThis.token,
+      server: process.env.SERVER,
+      token: process.env.TOKEN,
       tlsSkipVerify: true,
       gatewayGroup: 'default',
     });
