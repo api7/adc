8664eb4014840d8b64d0a0b16cfa36892a528abc
bzp2010 <bzp2010@apache.org>
2024-09-01 16:55:47 +0800

    feat: add credentials schema for linter

---
+++ apps/cli/src/linter/specs/consumer.spec.ts
@@ -0,0 +1,93 @@
+import * as ADCSDK from '@api7/adc-sdk';
+
+import { check } from '..';
+
+describe('Consumer Linter', () => {
+  const cases = [
+    {
+      name: 'should check consumer credentials (success)',
+      input: {
+        consumers: [
+          {
+            username: 'jack',
+            credentials: [
+              {
+                name: 'jack-1',
+                type: 'key-auth',
+                config: {
+                  key: 'jack',
+                },
+              },
+            ],
+          },
+        ],
+      } as ADCSDK.Configuration,
+      expect: true,
+      errors: [],
+    },
+    {
+      name: 'should check consumer credentials (miss config)',
+      input: {
+        consumers: [
+          {
+            username: 'jack',
+            credentials: [
+              {
+                name: 'jack-1',
+                type: 'key-auth',
+              },
+            ],
+          },
+        ],
+      } as ADCSDK.Configuration,
+      expect: false,
+      errors: [
+        {
+          code: 'invalid_type',
+          expected: 'object',
+          message: 'Required',
+          path: ['consumers', 0, 'credentials', 0, 'config'],
+          received: 'undefined',
+        },
+      ],
+    },
+    {
+      name: 'should check consumer credentials (unsupported type)',
+      //@ts-expect-error for test
+      input: {
+        consumers: [
+          {
+            username: 'jack',
+            credentials: [
+              {
+                name: 'jack-1',
+                type: 'none-auth',
+                config: {},
+              },
+            ],
+          },
+        ],
+      } as ADCSDK.Configuration,
+      expect: false,
+      errors: [
+        {
+          code: 'custom',
+          message:
+            'Consumer credential only supports "key-auth", "basic-auth", "jwt-auth" and "hmac-auth" types',
+          path: ['consumers', 0, 'credentials', 0, 'type'],
+        },
+      ],
+    },
+  ];
+
+  // test cases runner
+  cases.forEach((item) => {
+    it(item.name, () => {
+      const result = check(item.input);
+      expect(result.success).toEqual(item.expect);
+      if (!item.expect) {
+        expect(result.error.errors).toEqual(item.errors);
+      }
+    });
+  });
+});
