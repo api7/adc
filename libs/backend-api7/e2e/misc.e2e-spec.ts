30d497bf38ca0b3894f6c733d825f3596c4c8e2b
bzp2010 <bzp2010@apache.org>
2024-11-10 20:31:24 +0800

    test(api7): e2e custom id

--- libs/backend-api7/e2e/misc.e2e-spec.ts
+++ libs/backend-api7/e2e/misc.e2e-spec.ts
@@ -5,6 +5,7 @@ import {
   createEvent,
   deleteEvent,
   dumpConfiguration,
+  overrideEventResourceId,
   syncEvents,
 } from './support/utils';
 
@@ -61,6 +62,68 @@ describe('Miscellaneous', () => {
       syncEvents(backend, [
         deleteEvent(ADCSDK.ResourceType.ROUTE, routeName, serviceName),
         deleteEvent(ADCSDK.ResourceType.SERVICE, serviceName),
+      ]));
+
+    it('Dump again', async () => {
+      const result = (await dumpConfiguration(backend)) as ADCSDK.Configuration;
+      expect(result.services).toHaveLength(0);
+    });
+  });
+
+  describe('Sync resources with custom id', () => {
+    const routeName = 'Test Route';
+    const serviceName = 'Test Service';
+    const route = {
+      id: 'custom-route',
+      name: routeName,
+      uris: ['/test'],
+    };
+    const service = {
+      id: 'custom-service',
+      name: serviceName,
+      upstream: {
+        scheme: 'https',
+        nodes: [
+          {
+            host: 'httpbin.org',
+            port: 443,
+            weight: 100,
+          },
+        ],
+      },
+      routes: [route],
+    } as ADCSDK.Service;
+
+    it('Create services', async () =>
+      syncEvents(backend, [
+        overrideEventResourceId(
+          createEvent(ADCSDK.ResourceType.SERVICE, serviceName, service),
+          'custom-service',
+        ),
+        overrideEventResourceId(
+          createEvent(ADCSDK.ResourceType.ROUTE, routeName, route, serviceName),
+          'custom-route',
+        ),
+      ]));
+
+    it('Dump', async () => {
+      const result = (await dumpConfiguration(backend)) as ADCSDK.Configuration;
+      expect(result.services).toHaveLength(1);
+      expect(result.services[0]).toMatchObject(service);
+      expect(result.services[0]).toMatchObject(service);
+      expect(result.services[0].routes[0]).toMatchObject(route);
+    });
+
+    it('Delete service', async () =>
+      syncEvents(backend, [
+        overrideEventResourceId(
+          deleteEvent(ADCSDK.ResourceType.ROUTE, routeName, serviceName),
+          'custom-route',
+        ),
+        overrideEventResourceId(
+          deleteEvent(ADCSDK.ResourceType.SERVICE, serviceName),
+          'custom-service',
+        ),
       ]));
 
     it('Dump again', async () => {
