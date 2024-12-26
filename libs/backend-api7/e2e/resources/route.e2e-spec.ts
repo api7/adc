367cc10270834f7e0fe208501e939380f8933edb
bzp2010 <bzp2010@apache.org>
2024-12-26 19:20:00 +0800

    test: e2e

--- libs/backend-api7/e2e/resources/route.e2e-spec.ts
+++ libs/backend-api7/e2e/resources/route.e2e-spec.ts
@@ -72,4 +72,53 @@ describe('Route E2E', () => {
       expect(result.services).toHaveLength(0);
     });
   });
+
+  describe('Vars', () => {
+    const serviceName = 'test';
+    const service = {
+      name: serviceName,
+      upstream: {
+        scheme: 'https',
+        nodes: [
+          {
+            host: 'httpbin.org',
+            port: 443,
+            weight: 100,
+          },
+        ],
+      },
+      path_prefix: '/test',
+      strip_path_prefix: true,
+    } as ADCSDK.Service;
+    const route1Name = 'route1';
+    const route1 = {
+      name: route1Name,
+      uris: ['/route1'],
+      vars: [['remote_addr', '==', '1.1.1.1']],
+    } as ADCSDK.Route;
+
+    it('Create resources', async () =>
+      syncEvents(backend, [
+        createEvent(ADCSDK.ResourceType.SERVICE, serviceName, service),
+        createEvent(ADCSDK.ResourceType.ROUTE, route1Name, route1, serviceName),
+      ]));
+
+    it('Dump', async () => {
+      const result = (await dumpConfiguration(backend)) as ADCSDK.Configuration;
+      expect(result.services).toHaveLength(1);
+      expect(result.services[0]).toMatchObject(service);
+      expect(result.services[0].routes).toHaveLength(1);
+      expect(result.services[0].routes[0]).toMatchObject(route1);
+    });
+
+    it('Delete', async () =>
+      syncEvents(backend, [
+        deleteEvent(ADCSDK.ResourceType.SERVICE, serviceName),
+      ]));
+
+    it('Dump again (service should not exist)', async () => {
+      const result = (await dumpConfiguration(backend)) as ADCSDK.Configuration;
+      expect(result.services).toHaveLength(0);
+    });
+  });
 });
