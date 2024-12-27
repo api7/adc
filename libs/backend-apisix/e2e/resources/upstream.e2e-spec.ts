0450ffa0c24b2db8b6745546eea6535bb859103d
bzp2010 <bzp2010@apache.org>
2024-12-27 16:19:53 +0800

    test: fix

--- libs/backend-apisix/e2e/resources/upstream.e2e-spec.ts
+++ libs/backend-apisix/e2e/resources/upstream.e2e-spec.ts
@@ -1,18 +1,15 @@
 import * as ADCSDK from '@api7/adc-sdk';
-import { gte, lt } from 'semver';
 
 import { BackendAPISIX } from '../../src';
 import { server, token } from '../support/constants';
-import { conditionalDescribe, semverCondition } from '../support/utils';
 import {
   createEvent,
   deleteEvent,
   dumpConfiguration,
   syncEvents,
-  updateEvent,
 } from '../support/utils';
 
-describe('Consumer E2E', () => {
+describe('Upstream E2E', () => {
   let backend: BackendAPISIX;
 
   beforeAll(() => {
@@ -29,59 +26,29 @@ describe('Consumer E2E', () => {
       discovery_type: 'kubernetes',
       service_name: 'test',
     } as ADCSDK.Upstream;
-    const service1Name = 'service1';
-    const service1 = {
-      name: service1Name,
-      upstream: structuredClone(upstream),
-    } as ADCSDK.Service;
-    const service2Name = 'service2';
-    const service2 = {
-      name: service2Name,
+    const serviceName = 'service1';
+    const service = {
+      name: serviceName,
       upstream: structuredClone(upstream),
     } as ADCSDK.Service;
 
     it('Create services', async () =>
       syncEvents(backend, [
-        createEvent(ADCSDK.ResourceType.SERVICE, service1Name, service1),
-        createEvent(ADCSDK.ResourceType.SERVICE, service2Name, service2),
+        createEvent(ADCSDK.ResourceType.SERVICE, serviceName, service),
       ]));
 
     it('Dump', async () => {
       const result = (await dumpConfiguration(backend)) as ADCSDK.Configuration;
-      expect(result.services).toHaveLength(2);
-      expect(result.services[0]).toMatchObject(service2);
-      expect(result.services[1]).toMatchObject(service1);
-    });
-
-    it('Update service1', async () => {
-      service1.description = 'desc';
-      await syncEvents(backend, [
-        updateEvent(ADCSDK.ResourceType.SERVICE, service1Name, service1),
-      ]);
-    });
-
-    it('Dump again (service1 updated)', async () => {
-      const result = (await dumpConfiguration(backend)) as ADCSDK.Configuration;
-      expect(result.services[1]).toMatchObject(service1);
-    });
-
-    it('Delete service1', async () =>
-      syncEvents(backend, [
-        deleteEvent(ADCSDK.ResourceType.SERVICE, service1Name),
-      ]));
-
-    it('Dump again (service1 should not exist)', async () => {
-      const result = (await dumpConfiguration(backend)) as ADCSDK.Configuration;
       expect(result.services).toHaveLength(1);
-      expect(result.services[0]).toMatchObject(service2);
+      expect(result.services[0]).toMatchObject(service);
     });
 
-    it('Delete service2', async () =>
+    it('Delete service', async () =>
       syncEvents(backend, [
-        deleteEvent(ADCSDK.ResourceType.SERVICE, service2Name),
+        deleteEvent(ADCSDK.ResourceType.SERVICE, serviceName),
       ]));
 
-    it('Dump again (service2 should not exist)', async () => {
+    it('Dump again', async () => {
       const result = (await dumpConfiguration(backend)) as ADCSDK.Configuration;
       expect(result.services).toHaveLength(0);
     });
