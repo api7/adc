d3a9dd784c9479d8d1e4b33cc61cf524317e0c54
Zeping Bai <bzp2010@apache.org>
2025-01-24 10:28:47 +0800

    feat(core): add remote state file as experimental feature (#229)

--- apps/cli/src/command/index.ts
+++ apps/cli/src/command/index.ts
@@ -14,10 +14,22 @@ export const setupCommands = (): Command => {
   const program = new Command('adc');
 
   program
-    .description('API Declarative CLI (ADC) is a utility to manage API7 Enterprise and Apache APISIX declaratively.\n\nLearn more at: https://docs.api7.ai/enterprise/reference/adc')
+    .description(
+      'API Declarative CLI (ADC) is a utility to manage API7 Enterprise and Apache APISIX declaratively.\n\nLearn more at: https://docs.api7.ai/enterprise/reference/adc',
+    )
     .configureHelp({ showGlobalOptions: true })
     .passThroughOptions()
     .version('0.17.0', '-v, --version', 'display ADC version');
+
+  if (
+    process.env.ADC_EXPERIMENTAL_FEATURE_FLAGS &&
+    process.env.ADC_EXPERIMENTAL_FEATURE_FLAGS.includes('remote-state-file')
+  ) {
+    const desc =
+      'path of the remote state file, which will allow the ADC to skip the initial dump process and use the ADC configuration contained in the remote state file directly';
+    DiffCommand.option('--remote-state-file <file-path>', desc);
+    SyncCommand.option('--remote-state-file <file-path>', desc);
+  }
 
   program
     .addCommand(PingCommand)
