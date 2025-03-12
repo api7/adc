e28db87d2b5636c92d2d0e8f5af8b5b000e6a0dd
bzp2010 <bzp2010@apache.org>
2025-03-12 13:36:04 +0800

    fix: schema

--- libs/backend-api7/e2e/default-value.e2e-spec.ts
+++ libs/backend-api7/e2e/default-value.e2e-spec.ts
@@ -89,55 +89,6 @@ describe('Default Value', () => {
       expect(defaultValue).toMatchObject({
         core: {
           service: {
-            upstream: {
-              checks: {
-                active: {
-                  concurrency: 10,
-                  healthy: {
-                    http_statuses: [200, 302],
-                    interval: 1,
-                    successes: 2,
-                  },
-                  http_path: '/',
-                  https_verify_certificate: true,
-                  timeout: 1,
-                  type: 'http',
-                  unhealthy: {
-                    http_failures: 5,
-                    http_statuses: [429, 404, 500, 501, 502, 503, 504, 505],
-                    interval: 1,
-                    tcp_failures: 2,
-                    timeouts: 3,
-                  },
-                },
-                passive: {
-                  healthy: {
-                    http_statuses: [
-                      200, 201, 202, 203, 204, 205, 206, 207, 208, 226, 300,
-                      301, 302, 303, 304, 305, 306, 307, 308,
-                    ],
-                    successes: 5,
-                  },
-                  type: 'http',
-                  unhealthy: {
-                    http_failures: 5,
-                    http_statuses: [429, 500, 503],
-                    tcp_failures: 2,
-                    timeouts: 7,
-                  },
-                },
-              },
-              discovery_args: {},
-              hash_on: 'vars',
-              keepalive_pool: { idle_timeout: 60, requests: 1000, size: 320 },
-              name: 'default',
-              nodes: [{ priority: 0 }],
-              pass_host: 'pass',
-              retry_timeout: 0,
-              scheme: 'http',
-              timeout: { connect: 60, read: 60, send: 60 },
-              type: 'roundrobin',
-            },
             strip_path_prefix: true,
           },
           ssl: { client: { depth: 1 } },
