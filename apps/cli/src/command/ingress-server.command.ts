92949904ecbc7e809403314f0a12fedc22dfc101
bzp2010 <bzp2010@apache.org>
2025-07-29 17:46:35 +0800

    feat(core): configurable ingress server listen

--- apps/cli/src/command/ingress-server.command.ts
+++ apps/cli/src/command/ingress-server.command.ts
@@ -1,9 +1,22 @@
 import { ADCServer } from '../server';
-import { BaseCommand } from './helper';
+import { BaseCommand, BaseOptions } from './helper';
 
-export const IngressServerCommand = new BaseCommand('server').handle(
-  async () => {
-    await new ADCServer().start();
-    console.log('ADC server is running on: http://127.0.0.1:3000');
-  },
-);
+type IngressServerOptions = {
+  listen?: URL;
+} & BaseOptions;
+
+export const IngressServerCommand = new BaseCommand<IngressServerOptions>(
+  'server',
+)
+  .option<URL>(
+    '--listen <listen>',
+    'listen address of ADC server, the format is scheme://host:port',
+    (val) => new URL(val),
+    new URL('http://127.0.0.1:3000'),
+  )
+  .handle(async ({ listen }) => {
+    await new ADCServer({
+      listen,
+    }).start();
+    console.log(`ADC server is running on: ${listen.origin}`);
+  });
