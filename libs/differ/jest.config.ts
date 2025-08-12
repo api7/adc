4439b9ae5d229bf54d9881542125b60a215564f3
bzp2010 <bzp2010@apache.org>
2025-08-12 19:18:10 +0800

    debug

--- libs/differ/jest.config.ts
+++ libs/differ/jest.config.ts
@@ -1,8 +1,6 @@
 /* eslint-disable */
 import { readFileSync } from 'fs';
 
-console.log(process.env);
-
 // Reading the SWC compilation config for the spec files
 const swcJestConfig = JSON.parse(
   readFileSync(`${__dirname}/.spec.swcrc`, 'utf-8'),
