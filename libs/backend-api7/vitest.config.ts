7f00e22d3d0b40c65a5725fdb827344d140091ab
bzp2010 <bzp2010@apache.org>
2025-08-13 13:08:46 +0800

    move

---
+++ libs/backend-api7/vitest.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vitest/config';
+
+const isE2E = process.env.E2E === '1';
+
+export default defineConfig({
+  cacheDir: '../../node_modules/.vitest/libs/backend-api7',
+  test: {
+    globals: true,
+    environment: 'node',
+    include: [isE2E ? 'e2e/**/*.e2e-spec.ts' : 'test/**/*.spec.ts'],
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: isE2E
+        ? '../../coverage/libs/backend-api7/e2e'
+        : '../../coverage/libs/backend-api7',
+      provider: 'v8',
+    },
+    poolOptions: { forks: { singleFork: true } },
+    retry: 0,
+    ...(isE2E ? { globalSetup: '<rootDir>/e2e/support/global-setup.ts' } : {}),
+  },
+});
---
+++ libs/backend-apisix-standalone/vitest.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vitest/config';
+
+const isE2E = process.env.E2E === '1';
+
+export default defineConfig({
+  cacheDir: '../../node_modules/.vitest/libs/backend-apisix-standalone',
+  test: {
+    globals: true,
+    environment: 'node',
+    include: [isE2E ? 'e2e/**/*.e2e-spec.ts' : 'test/**/*.spec.ts'],
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: isE2E
+        ? '../../coverage/libs/backend-apisix-standalone/e2e'
+        : '../../coverage/libs/backend-apisix-standalone',
+      provider: 'v8',
+    },
+    poolOptions: { forks: { singleFork: true } },
+    retry: 0,
+  },
+});
