5f652b56a223bf89be6fc8f3b9d12f45ec313680
bzp2010 <bzp2010@apache.org>
2026-01-23 18:24:36 +0800

    fix: api7

--- libs/backend-api7/e2e/default-value.e2e-spec.ts
+++ libs/backend-api7/e2e/default-value.e2e-spec.ts
@@ -106,7 +106,97 @@ describe('Default Value', () => {
     'Check default value (>=3.8.0)',
     async () => {
       const defaultValue = await getDefaultValue(backend);
-      expect(defaultValue).toContainEqual('');
+      expect(defaultValue).toMatchObject({
+        core: {
+          consumer: {},
+          consumer_credential: {},
+          global_rule: {},
+          plugin_metadata: {},
+          route: {
+            priority: 0,
+            timeout: {
+              connect: 60,
+              read: 60,
+              send: 60,
+            },
+          },
+          service: {
+            strip_path_prefix: true,
+            upstream: {
+              discovery_args: {},
+              hash_on: 'vars',
+              keepalive_pool: {
+                idle_timeout: 60,
+                requests: 1000,
+                size: 320,
+              },
+              name: 'default',
+              nodes: [
+                {
+                  priority: 0,
+                },
+              ],
+              pass_host: 'pass',
+              retry_timeout: 0,
+              scheme: 'http',
+              timeout: {
+                connect: 60,
+                read: 60,
+                send: 60,
+              },
+              type: 'roundrobin',
+            },
+          },
+          ssl: {
+            certificates: [],
+            snis: [],
+          },
+          stream_route: {},
+          stream_service: {
+            upstream: {
+              hash_on: 'vars',
+              name: 'default',
+              nodes: [
+                {
+                  priority: 0,
+                },
+              ],
+              retry_timeout: 0,
+              scheme: 'tcp',
+              timeout: {
+                connect: 60,
+                read: 60,
+                send: 60,
+              },
+              type: 'roundrobin',
+            },
+          },
+          upstream: {
+            discovery_args: {},
+            hash_on: 'vars',
+            keepalive_pool: {
+              idle_timeout: 60,
+              requests: 1000,
+              size: 320,
+            },
+            name: 'default',
+            nodes: [
+              {
+                priority: 0,
+              },
+            ],
+            pass_host: 'pass',
+            retry_timeout: 0,
+            scheme: 'http',
+            timeout: {
+              connect: 60,
+              read: 60,
+              send: 60,
+            },
+            type: 'roundrobin',
+          },
+        },
+      });
     },
   );
 });
