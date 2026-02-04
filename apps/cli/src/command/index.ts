4de1075747468b2eb23b05d0cf9c11316ee45b98
Zeping Bai <zeping@api7.ai>
2026-02-04 14:10:45 +0800

    feat: bump to 0.24.2

--- apps/cli/src/command/index.ts
+++ apps/cli/src/command/index.ts
@@ -13,7 +13,7 @@ import { PingCommand } from './ping.command';
 import { SyncCommand } from './sync.command';
 import { configurePluralize } from './utils';
 
-const versionCode = '0.24.1';
+const versionCode = '0.24.2';
 
 // initialize dotenv
 dotenv.config();
