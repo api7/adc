ada94049e7967ee8cde3094999d976290acb3310
Zeping Bai <zeping@api7.ai>
2026-09-11 18:41:28 +0800

    feat: bump to 0.30.3 (#601)

--- apps/cli/src/command/index.ts
+++ apps/cli/src/command/index.ts
@@ -14,7 +14,7 @@ import { SyncCommand } from './sync.command';
 import { configurePluralize } from './utils';
 import { ValidateCommand } from './validate.command';
 
-const versionCode = '0.30.2';
+const versionCode = '0.30.3';
 
 function setupInternal() {
   // initialize dotenv
