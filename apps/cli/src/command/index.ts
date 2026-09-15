85cf37050ab18507033532e2b74d83031b8c564d
bzp2010 <bzp2010@apache.org>
2026-09-15 11:44:16 +0800

    feat: bump to 0.30.4

--- apps/cli/src/command/index.ts
+++ apps/cli/src/command/index.ts
@@ -14,7 +14,7 @@ import { SyncCommand } from './sync.command';
 import { configurePluralize } from './utils';
 import { ValidateCommand } from './validate.command';
 
-const versionCode = '0.30.3';
+const versionCode = '0.30.4';
 
 function setupInternal() {
   // initialize dotenv
